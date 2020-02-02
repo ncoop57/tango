2c6dc607e6573dba60a66a63c612e19951085dc7
ncoop57 <nacooper01@email.wm.edu>
2020-02-02 10:31:32 -0500

    Updated dependencies and setup to include fastai and have the proper settings

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM tensorflow/tensorflow:2.0.0rc0-gpu-py3-jupyter
 
 ADD ./requirements.txt .
 
-RUN pip install -r requirements.txt
+#RUN pip install -r requirements.txt
 RUN apt-get update -y && apt-get install git wget -y
 
-EXPOSE 8888 6006
+EXPOSE 8888
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-TAG=template
+TAG=tango
 
 if [ $# -eq 1 ]; then
 	if [ "$1" = "--build" ]; then
@@ -13,5 +13,6 @@ fi
 # Run the docker container. Add additional -v if
 # you need to mount more volumes into the container
 # Also, make sure to edit the ports to fix your needs.
-docker run -d --runtime=nvidia -v $(pwd):/tf/main \
-	-p 0.0.0.0:6008:6006 -p 8002:8888  $TAG
+docker run -d --runtime=nvidia -v $(pwd):/tf/main	\
+	-v /mnt/data/tango:/tf/data			\
+	-p 8004:8888  $TAG
